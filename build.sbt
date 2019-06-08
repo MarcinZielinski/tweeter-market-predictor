4cc79e6eba89d5b45d9619ee05fb23191eae71d2
Kamil Wróbel <sikorka1901@gmail.com>
2019-06-09 01:52:08 +0200

    Add sentiment analyzer

--- build.sbt
+++ build.sbt
@@ -4,4 +4,11 @@ version := "0.1"
 
 scalaVersion := "2.12.0"
 
-libraryDependencies ++= Seq("org.apache.spark" %% "spark-streaming" % "2.4.3", "org.apache.spark" %% "spark-core" % "2.4.3")
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-streaming" % "2.4.3",
+  "org.apache.spark" %% "spark-core" % "2.4.3",
+  "org.apache.spark" %% "spark-sql" % "2.4.3",
+  "org.apache.spark" %% "spark-mllib" % "2.4.3",
+  "org.apache.spark" % "spark-streaming-twitter_2.11" % "1.6.3",
+  "com.typesafe" % "config" % "1.3.4"
+)
--- src/main/scala/pl/suu/predicter/Main.scala
+++ src/main/scala/pl/suu/predicter/Main.scala
@@ -2,27 +2,31 @@ package pl.suu.predicter
 
 import org.apache.spark._
 import org.apache.spark.streaming._
+import pl.suu.predicter.sentiment.TweetSentimentAnalyzer
+import pl.suu.predicter.sentiment.mllib.SparkNaiveBayesModelCreator
 
 // Create a local StreamingContext with two working thread and batch interval of 1 second.
 // The master requires 2 cores to prevent from a starvation scenario.
 object Main extends App {
-  val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
-  val ssc = new StreamingContext(conf, Seconds(1))
-
-  // Create a DStream that will connect to hostname:port, like localhost:9999
-  val lines = ssc.socketTextStream("localhost", 9999)
-
-  // Split each line into words
-  val words = lines.flatMap(_.split(" "))
-
-  // Count each word in each batch
-  val pairs = words.map(word => (word, 1))
-  val wordCounts = pairs.reduceByKey(_ + _)
-
-  // Print the first ten elements of each RDD generated in this DStream to the console
-  wordCounts.print()
-
-  ssc.start() // Start the computation
-  ssc.awaitTermination() // Wait for the computation to terminate
+//  val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
+//  val ssc = new StreamingContext(conf, Seconds(1))
+//
+//  // Create a DStream that will connect to hostname:port, like localhost:9999
+//  val lines = ssc.socketTextStream("localhost", 9999)
+//
+//  // Split each line into words
+//  val words = lines.flatMap(_.split(" "))
+//
+//  // Count each word in each batch
+//  val pairs = words.map(word => (word, 1))
+//  val wordCounts = pairs.reduceByKey(_ + _)
+//
+//  // Print the first ten elements of each RDD generated in this DStream to the console
+//  wordCounts.print()
+//
+//  ssc.start() // Start the computation
+//  ssc.awaitTermination() // Wait for the computation to terminate
+  SparkNaiveBayesModelCreator.main(Array.empty)
+  TweetSentimentAnalyzer.main(Array.empty)
 
 }
