235d6954896ed3cb9e99bf56a06e5d134cd9138b
Marcin Zieliński <zielinskimarcin123@gmail.com>
2019-06-09 23:11:28 +0200

    Create spark stream from KGHM polish tweets from last 7 days

--- src/main/scala/pl/suu/predictor/Main.scala
+++ src/main/scala/pl/suu/predictor/Main.scala
@@ -3,68 +3,26 @@ package pl.suu.predictor
 import com.typesafe.config.ConfigFactory
 import org.apache.spark._
 import org.apache.spark.streaming._
-import org.apache.spark.streaming.twitter.TwitterUtils
-import twitter4j.auth.OAuthAuthorization
-import twitter4j.conf.ConfigurationBuilder
+import pl.suu.predictor.spark.TwitterReceiver
+import twitter4j.TwitterFactory
 
-// Create a local StreamingContext with two working thread and batch interval of 5 second.
+// Create a local StreamingContext with two working thread and batch interval of 1 second.
 // The master requires 2 cores to prevent from a starvation scenario.
 object Main extends App {
   val conf = new SparkConf().setMaster("local[2]").setAppName("TwitterData")
-  val ssc = new StreamingContext(conf, Seconds(60))
+  val ssc = new StreamingContext(conf, Seconds(1))
 
   val config = ConfigFactory.load()
-  val accessToken = config.getString("ACCESS_TOKEN_KEY")
-  val accessTokenSecret = config.getString("ACCESS_TOKEN_SECRET")
-  val consumerKey = config.getString("CONSUMER_KEY")
-  val consumerSecret = config.getString("CONSUMER_SECRET")
-  val filters = Seq("KGHM","cuprum", "Chludzinski", "Chludziński", "Chludzienskiemu", "Chludzińskiego")
-  val cb = new ConfigurationBuilder
-  cb.setDebugEnabled(true).setOAuthConsumerKey(consumerKey)
-    .setOAuthConsumerSecret(consumerSecret)
-    .setOAuthAccessToken(accessToken)
-    .setOAuthAccessTokenSecret(accessTokenSecret)
-  val auth = new OAuthAuthorization(cb.build)
-//  val tf = new TwitterFactory(cb.build())
-//  val twitter = tf.getInstance()
-//  val date = new Date().toInstant.minus(7, ChronoUnit.DAYS).toString
-//  val tweets = twitter.search(new Query().lang("en").since(date))
+  val filters = Seq("KGHM", "cuprum", "Chludzinski", "Chludziński", "Chludzienskiemu", "Chludzińskiego")
+  val tf = new TwitterFactory()
+  val twitter = tf.getInstance()
+  val stream = ssc.receiverStream(new TwitterReceiver(twitter))
 
-  val tweets = TwitterUtils.createStream(ssc, Some(auth), filters).filter(_.getLang == "en")
-  tweets.saveAsTextFiles("tweets", "json")
-
-  //  // Split each line into words
-  //  val words = lines.flatMap(_.split(" "))
-  //
-  //  // Count each word in each batch
-  //  val pairs = words.map(word => (word, 1))
-  //  val wordCounts = pairs.reduceByKey(_ + _)
-  //
-  //  // Print the first ten elements of each RDD generated in this DStream to the console
-  //  wordCounts.print()
+  stream.map(tweet => s"${tweet.getCreatedAt}: ${tweet.getText}").print(20)
 
   ssc.start()
   ssc.awaitTermination()
 
-//  SparkNaiveBayesModelCreator.main(Array.empty)
-//  TweetSentimentAnalyzer.main(Array.empty)
-//  def getFromKeywordSingleDay(i: DateTime,k: String, count: Int): String = {
-//    val consumer = new CommonsHttpOAuthConsumer(ConsumerKey, ConsumerSecret)
-//    consumer.setTokenWithSecret(AccessToken, AccessSecret)
-//    val url = "https://api.twitter.com/1.1/search/tweets.json?q=" + k + "&count=" + count + "&until=" + i.toString(StaticDateTimeFormat.forPattern("yyyy-MM-dd"))
-//    //println(url)
-//    val request = new HttpGet(url)
-//    consumer.sign(request)
-//    val client = HttpClientBuilder.create().build()
-//    val response = client.execute(request)
-//    IOUtils.toString(response.getEntity().getContent())
-//  }
-//
-//
-//  def getFromKeyword(k: String, count: Int = 90): String = {
-//    val today= DateTime.now
-//    //println(today.toString(StaticDateTimeFormat.forPattern("yyyy-MM-dd")))
-//    val ss = for (i <- 1 to 7) yield getFromKeywordSingleDay(today-i.days,k,count)
-//    ss.mkString("\n")
-//  }
+  //  SparkNaiveBayesModelCreator.main(Array.empty)
+  //  TweetSentimentAnalyzer.main(Array.empty)
 }
---
+++ src/main/scala/pl/suu/predictor/spark/TwitterReceiver.scala
@@ -0,0 +1,50 @@
+package pl.suu.predictor.spark
+
+import java.util.concurrent.ForkJoinPool
+
+import org.apache.spark.internal.Logging
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.receiver.Receiver
+import org.joda.time.DateTime
+import twitter4j.{Query, Status, Twitter}
+
+import scala.collection.JavaConverters._
+import scala.concurrent.{ExecutionContext, Future}
+
+class TwitterReceiver(twitter: Twitter)
+  extends Receiver[Status](StorageLevel.MEMORY_AND_DISK_2) with Logging {
+
+
+  def onStart() {
+    new Thread("Twitter Receiver") {
+      override def run() {
+        receive()
+      }
+    }.start()
+  }
+
+  def onStop() {
+    // There is nothing much to do as the thread calling receive() is designed to stop by itself
+  }
+
+  private def receive() {
+    try {
+      implicit val executionContext: ExecutionContext = ExecutionContext.fromExecutor(new ForkJoinPool(4))
+
+      for {
+        i <- 0 to 8
+        dateUntil = DateTime.now().minusDays(i).toString("yyyy-MM-dd")
+        dateSince = DateTime.now().minusDays(i + 1).toString("yyyy-MM-dd")
+      } {
+        for {
+          tw <- Future(twitter.search(new Query("KGHM OR cuprum OR Chludzinski OR Chludziński").lang("pl").since(dateSince).until(dateUntil).count(100)))
+        } yield store(tw.getTweets.asScala.iterator)
+      }
+
+    } catch {
+      case t: Throwable =>
+        // restart if there is error
+        restart("Error receiving data", t)
+    }
+  }
+}
