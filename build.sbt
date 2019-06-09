330fb3253e8aa4aeac739e5ffda8b5483700a478
Marcin Zieliński <zielinskimarcin123@gmail.com>
2019-06-09 19:31:13 +0200

    Real time tweets streaming

--- build.sbt
+++ build.sbt
@@ -9,10 +9,11 @@ libraryDependencies ++= Seq(
   "org.scalamock" %% "scalamock" % "4.1.0" % Test,
   "net.liftweb" %% "lift-json" % "3.3.0",
   "com.jsuereth" %% "scala-arm" % "2.0",
-"org.apache.spark" %% "spark-streaming" % "2.4.3",
+  "org.apache.spark" %% "spark-streaming" % "2.4.3",
   "org.apache.spark" %% "spark-core" % "2.4.3",
   "org.apache.spark" %% "spark-sql" % "2.4.3",
   "org.apache.spark" %% "spark-mllib" % "2.4.3",
   "org.apache.spark" % "spark-streaming-twitter_2.11" % "1.6.3",
+//  "org.apache.bahir" % "spark-streaming-twitter_2.11" % "2.3.3",
   "com.typesafe" % "config" % "1.3.4"
 )
--- src/main/scala/pl/suu/predictor/Main.scala
+++ src/main/scala/pl/suu/predictor/Main.scala
@@ -1,32 +1,70 @@
 package pl.suu.predictor
 
+import com.typesafe.config.ConfigFactory
 import org.apache.spark._
 import org.apache.spark.streaming._
-import pl.suu.predictor.sentiment.TweetSentimentAnalyzer
-import pl.suu.predictor.sentiment.mllib.SparkNaiveBayesModelCreator
+import org.apache.spark.streaming.twitter.TwitterUtils
+import twitter4j.auth.OAuthAuthorization
+import twitter4j.conf.ConfigurationBuilder
 
-// Create a local StreamingContext with two working thread and batch interval of 1 second.
+// Create a local StreamingContext with two working thread and batch interval of 5 second.
 // The master requires 2 cores to prevent from a starvation scenario.
 object Main extends App {
-//  val conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount")
-//  val ssc = new StreamingContext(conf, Seconds(1))
-//
-//  // Create a DStream that will connect to hostname:port, like localhost:9999
-//  val lines = ssc.socketTextStream("localhost", 9999)
-//
-//  // Split each line into words
-//  val words = lines.flatMap(_.split(" "))
-//
-//  // Count each word in each batch
-//  val pairs = words.map(word => (word, 1))
-//  val wordCounts = pairs.reduceByKey(_ + _)
-//
-//  // Print the first ten elements of each RDD generated in this DStream to the console
-//  wordCounts.print()
-//
-//  ssc.start() // Start the computation
-//  ssc.awaitTermination() // Wait for the computation to terminate
-  SparkNaiveBayesModelCreator.main(Array.empty)
-  TweetSentimentAnalyzer.main(Array.empty)
+  val conf = new SparkConf().setMaster("local[2]").setAppName("TwitterData")
+  val ssc = new StreamingContext(conf, Seconds(60))
 
+  val config = ConfigFactory.load()
+  val accessToken = config.getString("ACCESS_TOKEN_KEY")
+  val accessTokenSecret = config.getString("ACCESS_TOKEN_SECRET")
+  val consumerKey = config.getString("CONSUMER_KEY")
+  val consumerSecret = config.getString("CONSUMER_SECRET")
+  val filters = Seq("KGHM","cuprum", "Chludzinski", "Chludziński", "Chludzienskiemu", "Chludzińskiego")
+  val cb = new ConfigurationBuilder
+  cb.setDebugEnabled(true).setOAuthConsumerKey(consumerKey)
+    .setOAuthConsumerSecret(consumerSecret)
+    .setOAuthAccessToken(accessToken)
+    .setOAuthAccessTokenSecret(accessTokenSecret)
+  val auth = new OAuthAuthorization(cb.build)
+//  val tf = new TwitterFactory(cb.build())
+//  val twitter = tf.getInstance()
+//  val date = new Date().toInstant.minus(7, ChronoUnit.DAYS).toString
+//  val tweets = twitter.search(new Query().lang("en").since(date))
+
+  val tweets = TwitterUtils.createStream(ssc, Some(auth), filters).filter(_.getLang == "en")
+  tweets.saveAsTextFiles("tweets", "json")
+
+  //  // Split each line into words
+  //  val words = lines.flatMap(_.split(" "))
+  //
+  //  // Count each word in each batch
+  //  val pairs = words.map(word => (word, 1))
+  //  val wordCounts = pairs.reduceByKey(_ + _)
+  //
+  //  // Print the first ten elements of each RDD generated in this DStream to the console
+  //  wordCounts.print()
+
+  ssc.start()
+  ssc.awaitTermination()
+
+//  SparkNaiveBayesModelCreator.main(Array.empty)
+//  TweetSentimentAnalyzer.main(Array.empty)
+//  def getFromKeywordSingleDay(i: DateTime,k: String, count: Int): String = {
+//    val consumer = new CommonsHttpOAuthConsumer(ConsumerKey, ConsumerSecret)
+//    consumer.setTokenWithSecret(AccessToken, AccessSecret)
+//    val url = "https://api.twitter.com/1.1/search/tweets.json?q=" + k + "&count=" + count + "&until=" + i.toString(StaticDateTimeFormat.forPattern("yyyy-MM-dd"))
+//    //println(url)
+//    val request = new HttpGet(url)
+//    consumer.sign(request)
+//    val client = HttpClientBuilder.create().build()
+//    val response = client.execute(request)
+//    IOUtils.toString(response.getEntity().getContent())
+//  }
+//
+//
+//  def getFromKeyword(k: String, count: Int = 90): String = {
+//    val today= DateTime.now
+//    //println(today.toString(StaticDateTimeFormat.forPattern("yyyy-MM-dd")))
+//    val ss = for (i <- 1 to 7) yield getFromKeywordSingleDay(today-i.days,k,count)
+//    ss.mkString("\n")
+//  }
 }
